7e457a9e6729b881d01b932c3eb9f20c34f496e2
unknown <guilhermebrito@gec.inatel.br>
2025-10-27 15:11:41 -0300

    refactor: users_cache_repo for better testing

--- backend/app/repositories/users_cache_repository.py
+++ backend/app/repositories/users_cache_repository.py
@@ -1,14 +1,17 @@
 from datetime import datetime, timezone
-from app.core.mongo import db
-
+from motor.motor_asyncio import AsyncIOMotorDatabase
 class UserCacheRepo:
+
+    def __init__(self, db: AsyncIOMotorDatabase):
+        self.db = db
+
     # update a user cache or create if there is no cache for this user
-    async def upsert_user_cache(user_id: int, name: str, user_photo_url: str = None):
-        await db['Users_cache'].update_one(
+    async def upsert_user_cache(self, user_id: int, name: str, user_photo_url: str = None):
+        await self.db['Users_cache'].update_one(
             {'_id': user_id},
             {'$set': {'name': name, 'user_photo_url': user_photo_url, 'updated_at': datetime.now(timezone.utc)}},
             upsert=True
         )
 
-    async def get_user_cache(user_id: int):
-        return await db['Users_cache'].find_one({'_id': user_id})
+    async def get_user_cache(self, user_id: int):
+        return await self.db['Users_cache'].find_one({'_id': user_id})
